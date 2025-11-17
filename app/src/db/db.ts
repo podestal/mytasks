59b40457511d4dd33175b109f528e88f2ae7b347
podestal <luisrodriguezpodesta@gmail.com>
2025-11-17 06:19:02 -0500

    d1 instance created and tested

--- app/src/db/db.ts
+++ app/src/db/db.ts
@@ -5,6 +5,5 @@ const sqliteFilePath = process.env.SQLITE_PATH!
 
 // Create or open a local SQLite database file using Bun's native SQLite
 export const sqlite = new Database(sqliteFilePath)
-
 // Drizzle client for SQLite
 export const db = drizzle(sqlite)
